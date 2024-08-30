0434f064936b878fce971da82f820251cbf52cd4
oqashakhan <khanoqasha002@gmail.com>
2024-08-30 02:07:53 -0700

    Alert Add

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-AOS.init();
 
-AOS.init({
-  disable: false,
-  startEvent: 'DOMContentLoaded',
-  initClassName: 'aos-init', 
-  animatedClassName: 'aos-animate', 
-  useClassNames: false, 
-  disableMutationObserver: false, 
-  debounceDelay: 50, 
-  throttleDelay: 99,
+setTimeout(() => {
   
+    document.getElementById("form").classList.toggle("show");
+    
+}, 5000);
 
- 
-  offset: 120,
-  delay: 0, 
-  duration: 400, 
-  easing: 'ease', 
-  once: false,
-  mirror: false, 
-  anchorPlacement: 'top-bottom',
+let close = document.getElementById("close").addEventListener("click" ,()=>{
+    document.getElementById("form").classList.toggle("show");
+})
 
-});
+
